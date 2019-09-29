a3c195c90d81da22efbb4e9bdbabf4009e9bf970
”okabe” <toka.wshhnoabee@gmail.com>
2019-09-29 02:56:23 +0000

    Laravel

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+        @extends('layouts.profile')
+        
+        @section('title', '自己紹介')
+        
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>名前</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
