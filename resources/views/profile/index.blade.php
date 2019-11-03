896485b45672c97fe295e3194418ed072db2db4c
”okabe” <toka.wshhnoabee@gmail.com>
2019-11-03 02:20:22 +0000

    Laravel/PHP 18課題

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.frontprofile')
+
+@section('content')
+    <div class="container">
+        <hr color="#c0c0c0">
+        @if (!is_null($headline))
+            <div class="row">
+                <div class="headline col-md-10 mx-auto">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="caption mx-auto">
+                                
+                                <div class="title p-2">
+                                    <h1>{{ str_limit($headline->title, 70) }}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+        
+        <hr color="#c0c0c0">
+        <div class="row">
+            <div class="profiles col-md-8 mx-auto mt-3">
+                @foreach($profiles as $profile)
+                    <div class="profile">
+                        <div class="row">
+                            <div class="text col-md-6">
+                                <div class="date">
+                                    {{ $profile->updated_at->format('Y年m月d日') }}
+                                </div>
+                                
+                                <div class="name">
+                                    {{ str_limit($profile->name, 10) }}
+                                </div>
+                                <div class="gender">
+                                    {{ str_limit($profile->gender, 5) }}
+                                </div>
+                                <div class="hobby">
+                                    {{ str_limit($profile->hobby, 20) }}
+                                </div>
+                                <div class="introduction">
+                                    {{ str_limit($profile->introduction, 100) }}
+                                </div>
+                                
+                                
+                                
+                            </div>
+                          
+                        </div>
+                    </div>
+                    <hr color="#c0c0c0">
+                @endforeach
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
